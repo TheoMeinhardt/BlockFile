285ed1c1a964a3e38dd91edec3c1c389b122999f
MatthiasEisenhut <eisenhut.m03@htlwienwest.at>
2022-03-15 11:55:04 +0100

    added logo

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="text-white flex justify-between px-4 py-4">
-      <a href="/" class="text-lg font-bold">Logo</a>
+      <a href="/"><img src="" alt="@/assets/logo.svg" /></a>
 
       <ul class="hidden md:flex flex-row items-center align-middle gap-4">
         <a href="#" class="bg-pink-400 text-black px-4 py-2 rounded-md"><li>Home</li></a>
