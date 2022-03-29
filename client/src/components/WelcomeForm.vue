810e39570c0605bcc234003046fda6210f900a88
MatthiasEisenhut <eisenhut.m03@htlwienwest.at>
2022-03-29 12:55:19 +0200

    Welcome ohne alignment Login nicht designed Register nicht designed

---
+++ client/src/components/WelcomeForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- Muss noch aligned werden -->
+  <div
+    class="py-10 px-24 bg-white rounded-3xl w-4/12 flex flex-col justify-center max-h-fit mt-6 mb-5"
+  >
+    <a href="http://localhost:8080/#/home"
+      ><img src="../assets/logo.svg" alt="Logo" height="300" width="300"
+    /></a>
+    <h3 class="text-lg font-bold">Welcome to BlockFile</h3>
+    <p class="my-3">You need a account to access our services</p>
+    <router-link to="/login"
+      ><a class="px-10 py-3 bg-blue-500 rounded-md text-center">Login</a></router-link
+    >
+    <p class="my-3 text-center">Dont have an account?</p>
+    <router-link to="/register"
+      ><a class="px-10 py-3 shadow-md rounded-md text-center">Register</a></router-link
+    >
+  </div>
+</template>
---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-[url('@/assets/welcomeBackground_auto_x2.jpg')] min-h-screen">
+    <LoginForm></LoginForm>
+  </div>
+</template>
+
+<script setup>
+import LoginForm from '../components/LoginForm.vue';
+</script>
