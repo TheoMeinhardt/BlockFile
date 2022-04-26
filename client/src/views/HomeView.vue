e2d964bff34695f52a07ef8898f63691c9117466
MatthiasEisenhut <eisenhut.m03@htlwienwest.at>
2022-04-26 12:38:41 +0200

    waves placed

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,22 +1,21 @@
 <template>
   <div>
-    <div class="waveOne w-full h-[30rem]">
+    <div class="waveOne w-full h-[487px]">
       <NavBar></NavBar>
       <span class="text-3xl font-poppins ml-32 mt-32">Welcome to BlockFile Matthias!</span>
     </div>
-    <h1 class="bg-white text-3xl font-poppins ml-32">Upload your Files</h1>
-    <input type="file" />
+    <h1 class="bg-white text-3xl font-poppins ml-32 mt-32">Upload your Files</h1>
     <div
-      class="bg-gray-200 border-black flex flex-col text-center border w-80 border-dashed rounded-md h-32 content-center"
+      class="bg-gray-200 border-black flex flex-col text-center border w-80 border-dashed rounded-md h-32 content-center mb-32"
     >
       <i class="fa-solid fa-cloud-arrow-up text-4xl"></i>
       <h1 class="font-coolvetica text-4xl">drag & drop here</h1>
     </div>
-    <div class="waveTwo w-full">
-      <span class="text-3xl font-poppins">Welcome to BlockFile Matthias!</span>
+    <div class="waveTwo w-full h-[55rem]">
+      <span class="text-3xl font-poppins mt-32">Your uploaded Files</span>
     </div>
-    <div class="waveThree w-full">
-      <span class="text-3xl font-poppins ml-32">Welcome to BlockFile Matthias!</span>
+    <div class="waveThree w-full h-[55rem]">
+      <span class="text-3xl font-poppins ml-32">About us</span>
     </div>
   </div>
 </template>
